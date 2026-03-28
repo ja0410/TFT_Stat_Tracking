d9623e9f34e7d2b344858a94df51eb66a63c5a09
jallen27 <jdman543@gmail.com>
2026-03-28 18:08:45 -0500

    moved functions into seperate file for cleanliness, also made function to find average tft match placement

---
+++ TFT_Functions.py
@@ -0,0 +1,80 @@
+import requests
+
+
+
+
+
+# after getting username and tag, get summoners unique puuid
+def user_puuid(user, tag, api_key):
+    url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{user}/{tag}"
+
+    payload = {}
+    headers = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Origin": "https://developer.riotgames.com",
+    "X-Riot-Token": api_key
+}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()['puuid']
+
+# use the puuid gathered in above function to get user data (level, icon, etc.)
+def summoner_info(puuid, api_key):
+    url = f"https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/{puuid}"
+
+    payload = {}
+    headers = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Origin": "https://developer.riotgames.com",
+    "X-Riot-Token": api_key
+}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()
+
+# get last 20 matches of tft playing by summoner
+def tft_matches(puuid, api_key):
+    url = f"https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/{puuid}/ids?start=0&count=20"
+    payload = {}
+    headers = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Origin": "https://developer.riotgames.com",
+    "X-Riot-Token": api_key
+}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()
+
+# using match id of above function, get match data NOTE: large, 1600 line return, parse out only data you need for simplicity
+def match_info(match_id, api_key):
+    url = f'https://americas.api.riotgames.com/tft/match/v1/matches/{match_id}'
+
+    payload = {}
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Origin": "https://developer.riotgames.com",
+    "X-Riot-Token": api_key
+}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()
+
+# function with print statements for testing if needed
+def info_test(user, tag, api_key, puuid):
+    print(user_puuid(user, tag, api_key))
+    print(summoner_info(puuid, api_key))
+    print(tft_matches(puuid, api_key))
+
+# function for getting summoner placement in the last 20 games, as well as average placement
+def match_avg_placement(game_data, env):
+    matchplace = 0
+    for game in game_data:
+        game_results = game_data[game]
+        placement = game_results['placement']
+        matchplace += int(placement)
+        if env == 'test':
+            print(placement)
+    avg_place = matchplace / 20
+    print(avg_place)
--- main.py
+++ main.py
@@ -1,73 +1,10 @@
-import requests
 from configparser import ConfigParser
-import json
-from datetime import datetime
-
+from TFT_Functions import *
 config = ConfigParser()
 config.read('config.ini')
 
-# after getting username and tag, get summoners unique puuid
-def user_puuid(user, tag, api_key):
-    url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{user}/{tag}"
 
-    payload = {}
-    headers = {
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": api_key
-}
 
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response.json()['puuid']
-
-# use the puuid gathered in above function to get user data (level, icon, etc.)
-def summoner_info(puuid, api_key):
-    url = f"https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/{puuid}"
-
-    payload = {}
-    headers = {
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": api_key
-}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response.json()
-
-# get last 20 matches of tft playing by summoner
-def tft_matches(puuid, api_key):
-    url = f"https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/{puuid}/ids?start=0&count=20"
-    payload = {}
-    headers = {
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": api_key
-}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response.json()
-
-# using match id of above function, get match data NOTE: large, 1600 line return, parse out only data you need for simplicity
-def match_info(match_id, api_key):
-    url = f'https://americas.api.riotgames.com/tft/match/v1/matches/{match_id}'
-
-    payload = {}
-    headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": api_key
-}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response.json()
-
-# function with print statements for testing if needed
-def info_test(user, tag, api_key, puuid):
-    print(user_puuid(user, tag, api_key))
-    print(summoner_info(puuid, api_key))
-    print(tft_matches(puuid, api_key))
 
 
 if __name__ == '__main__':
@@ -98,7 +35,7 @@ if __name__ == '__main__':
     match_ids = tft_matches(puuid, api_key)
     # print out testing statements if testing along with normal prints
     if env == 'test':
-        info_test(user, tag, api_key, puuid, match_ids)
+        info_test(user, tag, api_key, puuid)
     # open a blank dictionary for summoner match data
     game_data = {}
     # make the length of match id's into a variable to keep track of how many need to be processed still
@@ -117,5 +54,7 @@ if __name__ == '__main__':
                 # every time a match is processed, take 1 off counter variable before moving to next match
                 matches_left -= 1
                 break
-    # print data to verify
-    print(game_data)
+    # print data to verify in test
+    if env == 'test':
+        print(game_data)
+    match_avg_placement(game_data, env)
